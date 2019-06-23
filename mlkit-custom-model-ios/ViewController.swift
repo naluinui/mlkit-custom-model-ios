ab23cc69ccaf0042b25f0c06319c546b084ec44d
Somjintana Korbut <nuisomjin@gmail.com>
2019-06-23 14:38:52 +0700

    initial commit

---
+++ mlkit-custom-model-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mlkit-custom-model-ios
+//
+//  Created by Somjintana Korbut on 23/6/2562 BE.
+//  Copyright © 2562 Nui.swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
