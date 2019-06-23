54bb57e2290aaf673238e46dee9dce75113dc88e
Somjintana Korbut <nuisomjin@gmail.com>
2019-06-23 15:20:18 +0700

    Add tflite models and model constants

--- mlkit-custom-model-ios/Constants.swift
+++ mlkit-custom-model-ios/Constants.swift
@@ -6,9 +6,29 @@
 //  Copyright © 2562 rfcx. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import FirebaseMLModelInterpreter
 
 struct Constants {
+    
+    public static let modelInfo = (name: "hourglass-model", extension: "tflite")
+    public static let labelsInfo = (name: "labels", extension: "txt", count: 2)
+    public static let topResultsCount: Int = 2
+    public static let componentCount: Int = 1
+    public static let batchSize: Int = 1
+    public static let imageSize = CGSize(width: 60, height: 250)
+    public static let elementType = ModelElementType.float32
+    public static let inputOutputIndex: UInt = 0
+    public static let inputDimensions = [
+        NSNumber(value: batchSize),
+        NSNumber(value: componentCount),
+        NSNumber(value: Int(imageSize.height)),
+        NSNumber(value: Int(imageSize.width)),
+    ]
+    public static let outputDimensions = [
+        NSNumber(value: batchSize),
+        NSNumber(value: labelsInfo.count)
+    ]
     
     static let images = [
         ImageDisplay(file: "000000151859.jpg", name: "Jumping"),
